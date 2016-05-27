e90160c872b05c5354b9889f1821a85a6a8c2025
Betsy Cowdery <emcowdery@gmail.com>
2016-05-27 00:59:26 -0400

    Updating small changes to original try.workflow

--- try.workflow.R
+++ try.workflow.R
@@ -5,6 +5,7 @@ library(coda)
 library(mvtnorm)
 library(data.table)
 
+# args will be cahin number / info about the type of run
 args = commandArgs(trailingOnly=TRUE)
 run_name <- args[1]
 
@@ -15,10 +16,10 @@ source(file.path("custom.jags.R"))
 ################################################################################
 ## JAGS runs setup 
 runs = list(
-  uni = FALSE,
-  uni.na = FALSE,
-  multi = FALSE,
-  multi.na = FALSE,
+  uni = FALSE,      #done
+  uni.na = FALSE,   #done
+  multi = FALSE,    #done
+  multi.na = FALSE, #done
   multi.pft = TRUE,
   multi.pft.na = TRUE)
 
@@ -32,10 +33,12 @@ thin <- 0
 
 ################################################################################
 ## Univariate Run
+
 model = "models/univarite.model.txt"
 uni.save <- c()
-if(runs$uni){
-  # Without na's
+
+if(runs$uni){ # Without na's
+  print("-------------------------")
   print(model)
   print(Sys.time())
   j.data <- try
@@ -46,8 +49,8 @@ if(runs$uni){
   out.uni.try <- custom.jags(model,data,inits,n.chains,burnin,n.update,var.names)
   uni.save <- c(uni.save, "out.uni.try")
 }
-if(runs$uni.na){
-  # With na's
+if(runs$uni.na){ # With na's
+  print("-------------------------")
   print(model)
   print(Sys.time())
   j.data <- try.na
@@ -58,7 +61,7 @@ if(runs$uni.na){
   out.uni.try.na <- custom.jags(model,data,inits,n.chains,burnin,n.update,var.names)
   uni.save <- c(uni.save, "out.uni.try.na")
 }
-################################################################################
+
 ## Save Data
 if(length(uni.save > 0)){
   t <- paste0("save(", paste(uni.save, collapse = ','),", file='",
@@ -69,33 +72,34 @@ if(length(uni.save > 0)){
 
 ################################################################################
 ## Multivariate Run
+
 model = "models/multivarite.model.txt"
 multi.save <- c()
-if(runs$multi){
-  # Without na's
+if(runs$multi){ # Without na's
+  print("-------------------------")
   print(model)
   print(Sys.time())
   j.data <- try
   N=dim(j.data)[1]; n=dim(j.data)[2]
   data = list(Y=j.data, N=N, n=n, Vsig = diag(n), mu0 = rep(0,n), Vmu = diag(.001,n))
   inits = list(mu = colMeans(j.data), prec.Sigma = solve(cov(j.data)))
-  var.names = c("mu", "Sigma")
+  var.names = c("mu")
   out.multi.try <- custom.jags(model,data,inits,n.chains,burnin,n.update,var.names)
   multi.save <- c(multi.save, "out.multi.try")
 }
-if(runs$multi.na){
-  # With na's
+if(runs$multi.na){ # With na's
+  print("-------------------------")
   print(model)
   print(Sys.time())
   j.data <- try.na
   N=dim(j.data)[1]; n=dim(j.data)[2]
   data = list(X=j.data, N=N, n=n, Vsig = diag(n), mu0 = rep(0,n), Vmu = diag(.001,n))
   inits = list(mu = colMeans(j.data), prec.Sigma = solve(cov(j.data)))
-  var.names = c("mu", "Sigma")
+  var.names = c("mu")
   out.multi.try.na <- custom.jags(model,data,inits,n.chains,burnin,n.update,var.names)
   multi.save <- c(multi.save, "out.multi.try.na")
 }
-################################################################################
+
 ## Save Data
 if(length(multi.save > 0)){
   t <- paste0("save(", paste(multi.save, collapse = ','),", file='",
@@ -104,19 +108,17 @@ if(length(multi.save > 0)){
   print("Done with multivariate")
 }
 
-################################################################################
-################################################################################
-## PFT Run without na's
 
-n.chains = 1
+################################################################################
+## PFT Run
+
+# New number of iterations because it needs thinning
 n.iter <- 600000
-n.update <- 20
-burnin <- 1000
 thin <- 40
 
 model = "models/multivariate.grp.model.txt"
 pft.save <- c()
-if(runs$multi.pft){
+if(runs$multi.pft){ # Without na's
   print("-------------------------")
   print(model)
   print(Sys.time())
@@ -144,34 +146,32 @@ if(runs$multi.pft){
   pft.save <- c(pft.save, "out.pft.try")
 }
 
-
-if(runs$multi.pft.na){
+if(runs$multi.pft.na){ # With na's 
   print("-------------------------")
   print(model)
   print(Sys.time())
-j.data <- try.na
-Nvars <- dim(j.data)[2]
-Nobs = dim(j.data)[1]
-Nvars = dim(j.data)[2]
-GroupNo = as.numeric(as.factor(try_full$pft[!is.na(try_full$pft)]))
-Ngroup = length(unique(GroupNo))
-Gamma = diag(Nvars)
-Omega = diag(Nvars)
-
-data = list(
-  X=j.data,
-  Nobs = Nobs,
-  Nvars = Nvars,
-  GroupNo = GroupNo,
-  Ngroup = Ngroup,
-  Gamma = Gamma,
-  Omega = Omega
-)
-inits = list(mu = colMeans(j.data), 
-             theta = as.matrix(pft_means))
-var.names = c("theta","mu")
-out.pft.try.na <- custom.jags(model,data,inits,n.chains,burnin,n.update,thin,var.names)
-pft.save <- c(pft.save, "out.pft.try.na")
+  j.data <- try.na
+  Nvars <- dim(j.data)[2]
+  Nobs = dim(j.data)[1]
+  Nvars = dim(j.data)[2]
+  GroupNo = as.numeric(as.factor(try_full$pft[!is.na(try_full$pft)]))
+  Ngroup = length(unique(GroupNo))
+  Gamma = diag(Nvars)
+  Omega = diag(Nvars)
+  data = list(
+    X=j.data,
+    Nobs = Nobs,
+    Nvars = Nvars,
+    GroupNo = GroupNo,
+    Ngroup = Ngroup,
+    Gamma = Gamma,
+    Omega = Omega
+  )
+  inits = list(mu = colMeans(j.data), 
+               theta = as.matrix(pft_means))
+  var.names = c("theta","mu")
+  out.pft.try.na <- custom.jags(model,data,inits,n.chains,burnin,n.update,thin,var.names)
+  pft.save <- c(pft.save, "out.pft.try.na")
 }
 print(Sys.time())
 print("Done with all the model runs! Saving output...")
@@ -186,4 +186,4 @@ if(length(pft.save > 0)){
 #######################################################################################
 print(Sys.time())
 print("All Done!")
-# Next run process.mcmc.R
+
