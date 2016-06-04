74a45220d8a283fb65b22ea393f3f31aa3232fef
Betsy Cowdery <emcowdery@gmail.com>
2016-06-04 04:00:21 -0400

    Adding qsub scripts for each model because ... I like it better that way.

---
+++ qsub.hier.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#$ -l h_rt=48:00:00
+#$ -N try.run.hier
+#$ -V
+#$ -j y
+#$ -pe omp 8 -q "geo*"
+#$ -v OMP_NUM_THREADS=8
+
+Rscript 01.run.models.R uni=FALSE uni.group=FALSE multi=FALSE multi.group=FALSE hier=TRUE with.na=FALSE n.chains=5
