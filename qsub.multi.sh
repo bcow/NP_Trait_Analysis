964fb56b181775e3cb17173eb28b271caf5db5ee
Alexey Shiklomanov (scc) <ashiklom@bu.edu>
2016-06-04 12:24:06 -0400

    Added additional qsub scripts

--- qsub.multi.sh
+++ qsub.multi.sh
@@ -7,4 +7,4 @@
 #$ -pe omp 8 -q "geo*"
 #$ -v OMP_NUM_THREADS=8
 
-Rscript 01.run.models.R uni=FALSE uni.group=FALSE multi=TRUE multi.group=TRUE hier=FALSE with.na=FALSE n.chains=5
+Rscript 01.run.models.R uni=FALSE uni.group=FALSE multi=TRUE multi.group=TRUE hier=FALSE with.na=FALSE n.chains=8
