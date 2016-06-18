c8ac44c0aba2a40cf747ee8379249f119114c19d
Betsy Cowdery <emcowdery@gmail.com>
2016-06-18 14:06:26 -0400

    Updating submit.all.sh

--- submit.all.sh
+++ submit.all.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-for q in qsub.*.na.sh; do
-    qsub $q
+for i in $(seq 0 35); do
+    qsub qsub.uni.na.sh $i
 done
+
+for i in $(seq 0 35); do
+    qsub qsub.multi.na.sh $i
+done
+
+qsub qsub.hier.na.sh
+
+
