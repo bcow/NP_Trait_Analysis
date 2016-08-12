7ab7b72148552dfeca1f3de20e9c0cac77cc3345
Alexey Shiklomanov (scc) <ashiklom@bu.edu>
2016-08-12 16:57:22 -0400

    Add missing data precisions as parameters

--- 01.run.model.R
+++ 01.run.model.R
@@ -43,7 +43,8 @@ for (model_arg in model.args){
     message(paste("Running", model_arg))
     model_type <- arg[1]
     pft_number <- as.numeric(arg[2])
-    out <- runModel(model_type, try_data, pft_number, n.chains)
+    out <- runModel(model_type, try_data, pft_number, n.chains, 
+                    tau_obvs_miss=0.001, tau_obvs_pres=1000)
     if(!all(is.error(out))){
         save(out, file = sprintf("%s/%s%s.Rdata", out_dir, model_arg, na))
     } else {
