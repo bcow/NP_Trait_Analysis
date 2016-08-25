ae181cdf603b00c8f6235026261200009df03003
Alexey Shiklomanov <ashiklom@bu.edu>
2016-08-25 13:49:27 -0400

    Fix output loading. Working 2D density plots.

--- results.2d.density.R
+++ results.2d.density.R
@@ -16,8 +16,10 @@ try_data <- loadTRYData("data/try.data.rds")
 obs.means.global <- try_data[, lapply(.SD, mean, na.rm=TRUE), 
                            .SDcols = traits] %>% c() %>% unlist()
 
+pairs_path <- "figures/alexey_pairs"
+dir.create(pairs_path)
 message("Creating global figure...")
-mypng("figures/alexey_pairs/00.global.png")
+mypng(file.path(pairs_path, "00.global.png"))
 pairs.density(uni.all.global$mu_trait, 
               multi.all.global$mu_trait, 
               hier.all$mu_trait, 
@@ -29,15 +31,16 @@ dev.off()
 uni.all.pft <- readRDS("processed_output/sims.uni_pft.rds")
 multi.all.pft <- readRDS("processed_output/sims.multi_pft.rds")
 for(i in 1:npft){
-    print(paste(i, pft.names[i]))
+    current_pft <- pft.names[i]
+    print(paste(i, current_pft))
     uni.mus <- uni.all.pft$mu_trait[[i]]
     multi.mus <- multi.all.pft$mu_trait[[i]]
-    hier.mus <- hier.all$mu_pft_trait[,i,]
-    obs.means <- try.na[pft == pft.names[i], 
-                        lapply(.SD, mean, na.rm=TRUE),
-                        .SDcols = traits] %>% c() %>% unlist()
+    hier.mus <- getPFTFromHier(hier.all$mu_pft_trait, current_pft)
+    obs.means <- try_data[pft == pft.names[i], 
+                          lapply(.SD, mean, na.rm=TRUE),
+                          .SDcols = traits] %>% c() %>% unlist()
 
-    mypng(sprintf("figures/alexey_pairs/%02d.pft.png", i))
+    mypng(file.path(pairs_path, sprintf("%02d.pft.png", i)))
     pairs.density(uni.mus, multi.mus, hier.mus, 
                   obs.means, 
                   main=paste(i, pft.names[i]))
