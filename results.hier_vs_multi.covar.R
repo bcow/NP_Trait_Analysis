5695ae9b36cd1215bb4868456c3723eceac6157d
Alexey Shiklomanov <ashiklom@bu.edu>
2016-08-25 14:09:16 -0400

    Fix code for corrplots

--- results.hier_vs_multi.covar.R
+++ results.hier_vs_multi.covar.R
@@ -1,23 +1,10 @@
-library(corrplot)
-library(R2jags)
-
-source("00.common.R")
-
-plot_covar <- function(fname, ...){
-    load(fname)                        # Object name is "out"
-    cov.global <- out$BUGSoutput$mean$Sigma_trait
-    rownames(cov.global) <- colnames(cov.global) <- traits
-    cor.global <- cov2cor(cov.global)
-    plt <- corrplot.mixed(cor.global, lower = "ellipse", upper = "number",
-                          mar=c(0,0,2,0), ...)
-    return(plt)
-}
+library(mvtraits)
 
 png("figures/cor.global.multi.png")
-plot_covar("output/multi.trait.na/multi.trait.na.Rdata", main="Multivariate")
+plot_covar("processed_output/sims.multi_global.rds", main="Multivariate")
 dev.off()
 
 png("figures/cor.global.hierarchical.png")
-plot_covar("output/hier.trait.pft.na/hier.trait.pft.na.Rdata", main="Hierarchical")
+plot_covar("processed_output/sims.hier.rds", main="Hierarchical")
 dev.off()
 
