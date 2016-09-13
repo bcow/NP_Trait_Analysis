5459fa8f08614be480436a595e7902d6f6f4a568
Alexey Shiklomanov (scc) <ashiklom@bu.edu>
2016-09-13 10:56:08 -0400

    Set higher starting iterations count

--- 01.run.model.R
+++ 01.run.model.R
@@ -43,7 +43,7 @@ for (model_arg in model.args){
     message(paste("Running", model_arg))
     model_type <- arg[1]
     pft_number <- as.numeric(arg[2])
-    out <- runModel(model_type, try_data, pft_number, n.chains)
+    out <- runModel(model_type, try_data, pft_number, n.chains, iter = 20000)
     if(!all(is.error(out))){
         save(out, file = sprintf("%s/%s%s.Rdata", out_dir, model_arg, na))
     } else {
